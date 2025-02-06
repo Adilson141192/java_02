1404477b5087dfba7af74eb4ff750919fd315ab2
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-02-06 10:39:45 -0300

    020625 aula02010

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,8 @@ public class Main {
                 Ano de lancemento
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
     }
 }
